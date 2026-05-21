aad2ddb81d1ae396239e7f9eeeae5a3617e6d236
Yoann Kermet <y.kermet@gmail.com>
2026-05-21 16:22:46 +0200

    work in progress...

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,30 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+
+    if @bookmark.save
+      redirect_to list_path(@list), notice: "Le film a bien été ajouté !"
+    else
+      render "lists/show", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @list = List.find(@bookmark.list_id)
+    @bookmark.destroy
+    redirect_to list_path(@list), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   resources :movies, only: %w[index]
-  resources :lists, only: %w[new create]
+  resources :lists, only: %w[new create index show] do
+    resources :bookmarks, only: %w[new create]
+  end
+
+  delete "bookmarks/:id", to: "bookmarks#destroy", as: :bookmark
 end
